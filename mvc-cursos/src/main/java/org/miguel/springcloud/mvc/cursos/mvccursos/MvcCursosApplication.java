d7129d49bec53bdacfe3f54a0b9774632ffc63ba
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2023-02-23 15:37:02 -0500

    creacion del micro servicio para cursos

---
+++ mvc-cursos/src/main/java/org/miguel/springcloud/mvc/cursos/mvccursos/MvcCursosApplication.java
@@ -0,0 +1,13 @@
+package org.miguel.springcloud.mvc.cursos.mvccursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcCursosApplication.class, args);
+	}
+
+}
---
+++ mvc-cursos/src/test/java/org/miguel/springcloud/mvc/cursos/mvccursos/MvcCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miguel.springcloud.mvc.cursos.mvccursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
