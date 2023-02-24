3b523d1b85e0716b024d27157f022ef09102f656
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2023-02-24 15:20:48 -0500

    creacion de la implementacion de los servicios del curso

---
+++ mvc-cursos/src/main/java/org/miguel/springcloud/mvc/cursos/mvccursos/services/CursoServiceImpl.java
@@ -0,0 +1,39 @@
+package org.miguel.springcloud.mvc.cursos.mvccursos.services;
+
+import org.miguel.springcloud.mvc.cursos.mvccursos.models.entity.Curso;
+import org.miguel.springcloud.mvc.cursos.mvccursos.repositories.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class CursoServiceImpl  implements CursoService{
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Curso> ListarCurso() {
+        return (List<Curso>) cursoRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Curso> BuscarCursoPorId(Long id) {
+        return cursoRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Curso GuardarCurso(Curso curso) {
+        return cursoRepository.save(curso);
+    }
+
+    @Override
+    @Transactional
+    public void EliminarCurso(Long id) {
+     cursoRepository.deleteById(id);
+    }
+}
