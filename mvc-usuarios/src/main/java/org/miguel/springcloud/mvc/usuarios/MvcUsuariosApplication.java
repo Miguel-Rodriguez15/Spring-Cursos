c65c54bcf6de446908e909001b93af4a60ea407f
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2023-03-14 16:14:08 -0500

    Conexion feign en usuarios con cursos
    Creacion del curso cliente rest para llamar el metodo de eliminar al usuario de la tabla de usuarios y de cursos

--- mvc-usuarios/src/main/java/org/miguel/springcloud/mvc/usuarios/MvcUsuariosApplication.java
+++ mvc-usuarios/src/main/java/org/miguel/springcloud/mvc/usuarios/MvcUsuariosApplication.java
@@ -2,7 +2,9 @@ package org.miguel.springcloud.mvc.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MvcUsuariosApplication {
 
