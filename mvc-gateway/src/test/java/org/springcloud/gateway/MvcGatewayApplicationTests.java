90bae87ad527b74e3e0a23e626d22b1a8ba969b9
Miguel-Rodriguez15 <migueldavidrodriguez2@gmail.com>
2023-08-01 15:41:05 -0500

    creacion del application.properties

---
+++ mvc-gateway/src/test/java/org/springcloud/gateway/MvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springcloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
