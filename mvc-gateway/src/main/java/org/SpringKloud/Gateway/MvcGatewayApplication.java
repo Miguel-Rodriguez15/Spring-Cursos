905a3432b86e477ce7ae33c8d4724d46e58df9a2
Miguel-Rodriguez15 <migueldavidrodriguez2@gmail.com>
2023-08-01 15:08:54 -0500

    creacion inicial de spring cloud gateway

---
+++ mvc-gateway/src/main/java/org/SpringKloud/Gateway/MvcGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.SpringKloud.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcGatewayApplication.class, args);
+	}
+
+}
---
+++ mvc-gateway/src/test/java/org/SpringKloud/Gateway/MvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.SpringKloud.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
